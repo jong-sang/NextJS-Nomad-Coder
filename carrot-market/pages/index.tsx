581c68139dcbaef4341e5c8c6c0cd9601c4dd3c1
jong-sang <jspark@zipida.com>
2022-03-04 18:15:25 +0900

    #4 study start

---
+++ carrot-market/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div className="bg-slate-400 px-10 ">1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  )
+}
+
+export default Home
